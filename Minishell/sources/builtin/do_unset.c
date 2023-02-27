8090671f9ef075280a723d63b550adcffa828849
Marcus vinicius Pimenta <mpimenta@c2t3s11.42.rio>
2023-02-27 11:01:14 -0300

    unset testado

--- Minishell/sources/builtin/do_unset.c
+++ Minishell/sources/builtin/do_unset.c
@@ -1,14 +1,16 @@
 #include "../../includes/minishell.h"
 
-int  count_strs(char **strs)
-{
-    int i;
+// int  count_strs(char **strs)
+// {
+//     int i;
 
-    i = 0;
-    while (strs[i] != NULL)
-     i++;
-    return (i);
-}
+//     i = 0;
+//     while (strs[i] != NULL)
+//      i++;
+//     return (i);
+// } 
+
+// COLOCAR NA .H PARA PODER USAR AQUI!!!
 
 char **unset_env(char **envp, int unset_i)
 {
@@ -20,7 +22,7 @@ char **unset_env(char **envp, int unset_i)
     amount_strs = count_strs(envp);
     cpy_envp = (char **)malloc(sizeof(char *) * amount_strs);
     while (++i < (amount_strs - 1) && i != unset_i)
-        cpy_envp[i] = strdup(envp[i]);
+        cpy_envp[i] = ft_strdup(envp[i]);
     cpy_envp[i] = NULL;
     return (cpy_envp);
 }
@@ -32,10 +34,10 @@ char    **do_unset(char **env, char *unset)
     char **new_env;
 
     i = -1;
-    len_unset = strlen(unset);
+    len_unset = ft_strlen(unset);
     while(env[++i])
     {
-        if(strncmp(unset, env[i], len_unset) == 0)
+        if(ft_strncmp(unset, env[i], len_unset) == 0)
         {
             new_env = unset_env(env, i);
             env = new_env;
