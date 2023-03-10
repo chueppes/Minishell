cb41244a0fa264040e6d53021d1cac65c7ddcc90
Marcus vinicius Pimenta <mpimenta@c2t3s4.42.rio>
2023-03-10 11:32:14 -0300

    unset com leak para tratar

--- Minishell/sources/builtin/do_unset.c
+++ Minishell/sources/builtin/do_unset.c
@@ -1,29 +1,19 @@
 #include "../../includes/minishell.h"
 
-char	**unset_env(char **envp, int unset_i, int i, int j)
+char	**unset_env(char **envp, int unset_i)
 {
 	char	**cpy_envp;
 	int		amount_strs;
+	int		i;
 
+	i = -1;
 	amount_strs = count_strs(envp);
-	cpy_envp = (char **)malloc(sizeof(char *) * amount_strs - 1);
-	while (i < amount_strs && i != unset_i)
-	{
-		cpy_envp[j] = ft_strdup(envp[i]);
-		free(envp[i]);
-		i++;
-		j++;
-	}
-	free(envp[i]);
-	i++;
-	while (i < amount_strs)
-	{
-		cpy_envp[j] = ft_strdup(envp[i]);
-		free(envp[i]);
-		j++;
-		i++;
-	}
-	cpy_envp[j] = NULL;
+	cpy_envp = (char **)malloc(sizeof(char *) * amount_strs);
+	while (++i < amount_strs && i != unset_i)
+		cpy_envp[i] = ft_strdup(envp[i]);
+	while (++i < amount_strs)
+		cpy_envp[i - 1] = ft_strdup(envp[i]);
+	cpy_envp[i - 1] = NULL;
 	return (cpy_envp);
 }
 
@@ -44,6 +34,6 @@ void	do_unset(char **env, char *unset, t_data *mini)
 	while (env[++i])
 	{
 		if (ft_strncmp(unset, env[i], j) == 0)
-			mini->minishell_envp = unset_env(env, i, 0, 0);
+			mini->minishell_envp = unset_env(env, i);
 	}
 }
